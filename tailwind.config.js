2006f1b8811efec3c279b2a35e4ae1b7d89bd5de
div0021 <div2wk@gmail.com>
2023-05-10 01:27:05 +0530

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    screens:{
+      'mini': '350px',
+      'x-short':'370px',
+      'short':"394px",
+      'xs': '450px',
+      "md":'768px',
+      "lg":"1024px",
+      'xl':'1280px'
+  
+    },
+    fontFamily:{
+      "mont":['Montserrat', 'sans-serif'],
+      "lora":['Lora', 'serif'],
+      "crimson":['Crimson Pro', 'serif']
+    },
+    keyframes:{
+      myAnimation:{
+        '0%':{
+          transform:'translateX(100rem)',
+          opacity:'0',
+        },
+        '50%':{
+          transform:'scale(1.5)',
+        },
+        '100%':{
+          transform:'translateX(0)',
+          opacity:'1'
+        }
+      },
+   
+    },
+    animation:{
+      myAnimation:'myAnimation 2s ease-in-out',
+      secAnimation:'myAnimation 2s ease-in-out 1s forwards'
+      // {
+      //   'animation-timing-function':'ease-in-out',
+      //   'animation-duration':'2s',
+      //   'animation-name':'myAnimation',
+      //   'animation-delay':'1s'
+
+      // }
+    }
+  },
+  plugins: [],
+}
+
